8d2fbf85642d35ab2d303a3edeb419b1c90aa427
Matt Crowe <github.mpc@zym.me>
2017-09-08 13:59:15 +0000

    tag batch cmd

---
+++ app/Console/Commands/TagCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Services;
+
+class TagCommand extends BaseCommand
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tag {--p|packages=} {--t|tag=} {--m|message=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->git()->action('tag', $this->options());
+    }
+
+}
